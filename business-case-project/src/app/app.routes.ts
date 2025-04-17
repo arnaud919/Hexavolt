c33d44e593ef1b6fc08f27cf2f772c5f5f1e5d97
ArnaudHB <arnaudcolombe9@gmail.com>
2025-04-17 16:37:46 +0200

    index page, library Daisy UI

--- business-case-project/src/app/app.routes.ts
+++ business-case-project/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { ReserveStationComponent } from './reserve-station/reserve-station.compo
 import { RentStationComponent } from './rent-station/rent-station.component';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
@@ -11,4 +14,7 @@ export const routes: Routes = [
     { path: 'louer_une_borne', component: RentStationComponent},
     { path: 'connexion', component: LoginComponent},
     { path: 'inscription', component: SigninComponent},
+    { path: 'mentions_legales', component: LegalNoticeComponent},
+    { path: 'politique_de_confidentialite', component: PrivacyPolicyComponent},
+    { path: 'contact', component: ContactComponent},
 ];
