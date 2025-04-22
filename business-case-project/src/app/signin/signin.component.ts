d32a350a10d455be11ac616c1c64d7f443c23e21
ArnaudHB <arnaudcolombe9@gmail.com>
2025-04-22 08:31:50 +0200

    front end signin and login

--- business-case-project/src/app/signin/signin.component.ts
+++ business-case-project/src/app/signin/signin.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
